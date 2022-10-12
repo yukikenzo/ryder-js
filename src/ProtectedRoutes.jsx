9286ad74fea102a0f5445a0c226c722a7d60db72
yukikenzo <gentlejp1122@gmail.com>
2022-10-12 13:36:00 +0200

    some security stuff

--- src/ProtectedRoutes.jsx
+++
@@ -1,6 +0,0 @@
-import { Navigate, Outlet } from "react-router-dom";
-import React from 'react'
-
-export default function ProtectedRoutes({isAuth}) {
-    return isAuth ? <Outlet /> : <Navigate to="/login" />;
-}
