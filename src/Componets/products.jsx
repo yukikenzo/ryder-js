360afa53e618187882ebc65698a07a0d10c9c31f
yukikenzo <gentlejp1122@gmail.com>
2022-10-14 19:11:06 +0200

    solving overlapping problem

--- src/Componets/products.jsx
+++ src/Componets/products.jsx
@@ -10,13 +10,20 @@ export default function Products({ product }) {
     navigate(`/product/${product.id}`, {
       state: product
     })
+    window.scroll({
+      top: 0,
+      left: 0,
+      behavior: 'smooth'
+    });
   }
 
   return (
     <>
       <div className='product_image'>
-        <img className='default_image' src={product.img1} alt="clothes" />
-        <img onClick={passState} className='alt_image' src={product.img2} alt="link clothes" />
+        <div className='product_image_container'>
+          <img className='default_image' src={product.img1} alt="clothes" />
+          <img onClick={passState} className='alt_image' src={product.img2} alt="link clothes" />
+        </div>
         <p className='clothes_name'>{product.name}</p>
         <h4 className='clothes_price'>{'$' + product.price + '.00'}</h4>
       </div>
